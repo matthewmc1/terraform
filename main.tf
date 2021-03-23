dabf0a7365c714d36371399125548983c3304b75
Matthew McGibbon <matthewmcgibbon@outlook.com>
2021-03-23 19:01:18 +0000

    adding virtual machine and public ip to the NIC

--- main.tf
+++ main.tf
@@ -21,6 +21,17 @@ resource "azurerm_subnet" "internal" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+
+resource "azurerm_public_ip" "main" {
+  name                = "publicip"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  allocation_method   = "Static"
+
+  tags = {
+    environment = "Test"
+  }
+}
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
   resource_group_name = azurerm_resource_group.main.name
@@ -30,5 +41,33 @@ resource "azurerm_network_interface" "main" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = "$azurerm_public_ip.main.id"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "main" {
+  name                = "example-machine"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "$var.password"
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+
+  priority        = "Spot"
+  eviction_policy = "Deallocate"
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,15 @@
 variable "prefix" {
   description = "The prefix which should be used for all resources in this example"
-  default = "test"
+  default  = "vm"
 }
 
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
   default = "West Europe"
+}
+
+
+variable "password" {
+  description = "Default password used for VM's"
+  default = "Password123" #update this on your 
 }
